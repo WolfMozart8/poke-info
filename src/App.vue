00d3cb6f5c0e34ffab8aeb6db9b7428f18fb7ec6
Felipe <riopipe8@gmail.com>
2023-05-17 18:59:06 -0400

    main app little progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import card from './components/card.vue';
+
+
 </script>
 
 <template>
-  <card />
+  <div>H</div>
+  <card :pokemonId="2" />
+  <card :pokemonId="65" />
+  <card :pokemonId="75" />
 </template>
 
 <style scoped>
--- src/components/card.vue
+++ src/components/card.vue
@@ -6,10 +6,15 @@ export default {
             pokemon: null,
         }
     },
+    props : {
+        pokemonId : Number
+    },
+    
     methods: {
         async fetchPokemon() {
+            console.log(this.pokemonId)
             try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`);
 
                 if (!response.ok) {
                     throw new Error('No se pudo obtener el Pokémon.');
@@ -19,7 +24,7 @@ export default {
                 console.log(data)
                 this.pokemon = data
                 
-                const responseInfo = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.id}/`)
+                const responseInfo = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.pokemonId}/`)
 
                 if (!responseInfo.ok) {
                     throw new Error('No se pudo obtener el Pokémon.');
@@ -34,20 +39,20 @@ export default {
             }
         }
     },
-    async fetchInfo(pokemon) {
-        try {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`)
+    // async fetchInfo(pokemon) {
+    //     try {
+    //         const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`)
 
-            if (!response.ok) {
-                    throw new Error('No se pudo obtener el Pokémon.');
-                }
-            const data = await response.json();
+    //         if (!response.ok) {
+    //                 throw new Error('No se pudo obtener el Pokémon.');
+    //             }
+    //         const data = await response.json();
 
-            this.info = data
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    //         this.info = data
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 }
 </script>
 
@@ -57,13 +62,24 @@ export default {
         <img class="pokemon-image" :src="pokemon.sprites.front_default" alt="Imagen del Pokémon">
         <div class="pokemon-info">
             <h2 class="pokemon-name">{{ pokemon.name }}</h2>
+            <h2 v-if="info" class="pokemon-name-jp">{{ info.names[0].name }}</h2>
             <p class="pokemon-id">ID: {{ pokemon.id }}</p>
             <p v-if="info" class="pokemon-description">{{ info.flavor_text_entries[0].flavor_text }}</p>
         </div>
     </div>
+    <div v-else>...loading</div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Rampart+One&display=swap');
+
+.pokemon-name-jp {
+    font-family: 'Rampart One', cursive;
+    position: absolute;
+    writing-mode: tb;
+    top: 20px;
+    right: 0px;
+}
 .pokemon-card {
     width: 300px;
     border: 1px solid #ccc;
@@ -73,14 +89,18 @@ export default {
     flex-direction: column;
     align-items: center;
     text-align: center;
+    position: relative;
+    background-color: #F8F8F8;
 }
 
 .pokemon-image {
     width: 150px;
     height: auto;
-    /* border-radius: 50%; */
+    border-radius: 10px;
     object-fit: cover;
     margin-bottom: 10px;
+    background: url(../../src/assets/img/bg-grass.png);
+    background-size: cover;
 }
 
 .pokemon-info {
@@ -90,15 +110,19 @@ export default {
 .pokemon-name {
     font-size: 20px;
     margin: 0;
+    color: #333333;
 }
 
 .pokemon-id {
     font-size: 14px;
     margin: 5px 0;
+    color: #666666;
 }
 
 .pokemon-description {
     font-size: 14px;
     margin: 10px 0;
+    color: #888888;
+
 }
 </style>
